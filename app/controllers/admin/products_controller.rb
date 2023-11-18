a2a9d3dc215c3b86adca00b43c02ff3c3850c797
Gregory Dechant <dechantg@gmail.com>
2023-11-18 00:12:27 +0000

    added basic password health to admin products page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  before_action :authenticate
+
+
   def index
     @products = Product.order(id: :desc).all
   end
@@ -25,6 +28,12 @@ class Admin::ProductsController < ApplicationController
   end
 
   private
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
 
   def product_params
     params.require(:product).permit(
