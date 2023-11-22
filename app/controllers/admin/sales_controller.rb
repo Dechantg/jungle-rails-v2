2221899737e05421f575b639030d90d47e8cd6cf
Gregory Dechant <dechantg@gmail.com>
2023-11-22 23:13:26 +0000

    cleaned up some code. added create sale functionality for admin

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -5,6 +5,32 @@ class Admin::SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
+  def sale_params
+    params.require(:sale).permit(:name, :percent_off, :starts_on, :ends_on)
+  end
   
   end
   
