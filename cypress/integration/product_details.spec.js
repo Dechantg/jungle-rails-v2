5a60fbfb00e65a3310ff3645b529f01f18d3c686
Gregory Dechant <dechantg@gmail.com>
2023-11-22 00:37:58 +0000

    cleaned up cypress product details test to make sure the vlidation checks the webpage against the href

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -5,12 +5,16 @@
 it("Confirm user can click on product link", () => {
   cy.visit("/");
   cy.get('article').then(($article) => {
-    cy.wrap($article).first('a').then(($link) => {
-      const productPath = $link.attr('href');
-      cy.wrap($link).then((link) => console.log(link));
-      cy.wrap($link).find('img').click();
-    
+
+    cy.wrap($article).find('a').first().then(($link) => {
+
+      cy.wrap($link).invoke('attr', 'href').then((href) => {
+
+        cy.wrap($link).find('img').click();
+
+        cy.url().should('include', href);
+
+      });
     });
   });
-
 });
